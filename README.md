0a70a39833f770f37b98e62edf5baaf76d3d1cb6
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-02-11 18:07:38 +0000

    Adding deployment script

--- README.md
+++ README.md
@@ -33,7 +33,21 @@ For ease of configuration, live tests have been added in a different file. Make 
 
 ## Deploy
 
-You can deploy your own version using `yarn deploy:$network_name`
+To deploy, please fill the `.env` file -> `cp .env-template .env`:
+
+```js
+RPC_URL_GOERLI=""
+GOERLI_LINK="0x326C977E6efc84E512bB9C30f76E30c160eD06FB"
+GOERLI_VRF_WRAPPER="0x708701a1DfF4f478de54383E49a627eD4852C816"
+GOERLI_VRF_COORDINATOR="0x2ca8e0c643bde4c2e08ab1fa0da3401adad7734d"
+GOERLI_LINK_WHALE="0x4a3df8cae46765d33c2551ff5438a5c5fc44347c"
+WETH_ADDR="0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"
+VRF_CALLBACK_GAS="100000"
+VRF_RETRIES="3"
+LINK_FEE="250000000000000000"
+```
+
+You can deploy your own version using `yarn deploy:$network_name` for instance (for now) you can deploy to goerli using `yarn deploy:goerli`. To test deployment locally you can use `yarn deploy:test`.
 
 ## Safety
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,40 @@
-const deploy = async () => {
+import { ethers } from "hardhat"
 
+const deploy = async () => {
+    if (
+        !process.env.GOERLI_LINK || 
+        !process.env.GOERLI_VRF_WRAPPER || 
+        !process.env.WETH_ADDR || 
+        !process.env.VRF_CALLBACK_GAS ||
+        !process.env.VRF_RETRIES ||
+        !process.env.LINK_FEE
+    ) {
+        throw new Error("Missing config")
+    }
+    
+    const LINK_FEE = process.env.LINK_FEE
+    const LINK_ADDR = process.env.GOERLI_LINK
+    const VRF_WRAPPER_ADDR = process.env.GOERLI_VRF_WRAPPER
+    const WETH = process.env.WETH_ADDR
+    const CALLBACK_GAS_LIMIT = process.env.VRF_CALLBACK_GAS
+    const VRF_RETRIES = process.env.VRF_RETRIES
+
+    const deployer = (await ethers.getSigners())[0]
+
+    const raffleFiFactory = await ethers.getContractFactory("RaffleFi", deployer)
+    const raffleFi = await raffleFiFactory.deploy(
+        WETH,
+        LINK_ADDR,
+        VRF_WRAPPER_ADDR,
+        1, // 1 random number needed only
+        CALLBACK_GAS_LIMIT,
+        VRF_RETRIES,
+        LINK_FEE
+    )
+
+    await raffleFi.deployed()
+
+    console.log("RaffleFi contract deployed at", raffleFi.address)
 }
 
 deploy().catch()
